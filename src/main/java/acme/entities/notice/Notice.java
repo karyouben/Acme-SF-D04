cac21ed096eb954c6799608fe7e9e9543efb64fe
Ahydul <ahydul1@gmail.com>
2024-03-05 18:06:10 +0100

    Add banner and notice entities

---
+++ src/main/java/acme/entities/notice/Notice.java
@@ -0,0 +1,55 @@
+
+package acme.entities.notice;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PastOrPresent;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.client.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Notice extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	protected static final long	serialVersionUID	= 1L;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@PastOrPresent
+	@NotNull
+	protected Date				instantiation;
+
+	@NotBlank
+	@Length(max = 75)
+	protected String			title;
+
+	@NotBlank
+	@Length(max = 75)
+	protected String			author;
+
+	@NotBlank
+	@Length(max = 100)
+	protected String			message;
+
+	@URL
+	@Length(max = 255)
+	protected String			link;
+
+	@Email
+	@Length(max = 255)
+	protected String			email;
+
+}
