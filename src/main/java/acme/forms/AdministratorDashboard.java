a9eb53aa77148147a66bbcd4f670c63e9da1f2c1
tomhuecal <tomihc123@gmail.com>
2024-03-06 11:54:42 +0100

    Added adminsitrator data, dashboard and banner type

---
+++ src/main/java/acme/forms/AdministratorDashboard.java
@@ -0,0 +1,34 @@
+
+package acme.forms;
+
+import java.util.Map;
+
+import acme.client.data.AbstractForm;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AdministratorDashboard extends AbstractForm {
+	// Serialisation identifier -----------------------------------------------
+
+	protected static final long		serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	// Total number of principals with each role.
+	private Map<String, Integer>	totalUsers;
+
+	// ratio of notices with both an email address and a link;
+	private Double					linkAndEmailNoticesRatio;
+
+	// ratios of critical and non-critical objectives
+	private Double					criticalAndNonCriticalObjetives;
+
+	// average, minimum, maximum, and standard deviation of the value in the risks; 
+	private Map<String, Statistic>	risk;
+
+	// average, minimum, maximum, and standard deviation of the number of claims posted over the last 10 weeks
+	private Statistic				claimsInLast10WeeksData;
+
+}
