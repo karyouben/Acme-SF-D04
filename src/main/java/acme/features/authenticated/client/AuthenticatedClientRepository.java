f7cf21eb5e9e3c276acab9f9ae8ae4600b1360b6
tomhuecal <tomihc123@gmail.com>
2024-04-16 16:10:53 +0200

    Added more files

---
+++ src/main/java/acme/features/authenticated/client/AuthenticatedClientRepository.java
@@ -0,0 +1,6 @@
+package acme.features.authenticated.client;
+
+
+public interface AuthenticatedClientRepository {
+
+}
---
+++ src/main/java/acme/features/authenticated/client/AuthenticatedClientUpdateService.java
@@ -0,0 +1,6 @@
+package acme.features.authenticated.client;
+
+
+public class AuthenticatedClientUpdateService {
+
+}
