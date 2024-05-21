e2ee015016ec1592ffd9075da09bca5736e65744
Ahydul <ahydul1@gmail.com>
2024-05-21 09:55:00 +0200

    change tests

--- src/main/java/acme/features/manager/assignment/ManagerAssignmentShowService.java
+++ src/main/java/acme/features/manager/assignment/ManagerAssignmentShowService.java
@@ -28,7 +28,7 @@ public class ManagerAssignmentShowService extends AbstractService<Manager, Assig
 		final Principal principal = super.getRequest().getPrincipal();
 		final int userAccountId = principal.getAccountId();
 
-		final boolean authorise = assignment != null && assignment.getProject().getManager().getUserAccount().getId() == userAccountId;
+		final boolean authorise = assignment != null && assignment.getProject().getManager().getUserAccount().getId() == userAccountId && assignment.getProject().isDraftMode();
 
 		super.getResponse().setAuthorised(authorise);
 	}
